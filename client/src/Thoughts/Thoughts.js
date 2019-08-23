1942302e3c26631b9ca3ca2547b1381cd5daf889
Cragady <pinkdotdreamer@gmail.com>
2019-08-23 17:00:31 -0600

    styling, changed background of app, changed header to be slightly transparent

--- client/src/Thoughts/Thoughts.js
+++ client/src/Thoughts/Thoughts.js
@@ -5,14 +5,16 @@ export const Thoughts = props => {
     return(
         <section className="Thoughts">
             <section className="thought-card rounded">
-                <div className="img-name-container">
+                <section className="img-name-container">
                     <div className="img-container m-3 rounded">
                         {props.src && <img src={props.src} alt={props.name} />}
                     </div>
 
                     {/* {props.name && <p>{props.name}</p>} */}
-                    <p>{'Test Name'}</p>
-                </div>
+                    <div className="text-containers">
+                        <p>{'Test Name'}</p>
+                    </div>
+                </section>
 
                 {props.currentBeer && <p>Current Beer: {props.currentBeer}</p>}
                 {props.currentThought && <p>Current Thought: {props.currentThought}</p>}

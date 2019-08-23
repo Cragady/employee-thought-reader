9ef585e9aca46947da163f2c5bf62fb467f36ebf
Cragady <pinkdotdreamer@gmail.com>
2019-08-23 16:47:12 -0600

    some styling

--- client/src/Thoughts/Thoughts.js
+++ client/src/Thoughts/Thoughts.js
@@ -3,18 +3,21 @@ import './Thoughts.css';
 
 export const Thoughts = props => {
     return(
-        <section>
-            <div className="img-name-container">
-                <div className="img-container m-3 rounded">
-                    {props.src && <img src={props.src} alt={props.name} />}
+        <section className="Thoughts">
+            <section className="thought-card rounded">
+                <div className="img-name-container">
+                    <div className="img-container m-3 rounded">
+                        {props.src && <img src={props.src} alt={props.name} />}
+                    </div>
+
+                    {/* {props.name && <p>{props.name}</p>} */}
+                    <p>{'Test Name'}</p>
                 </div>
 
-                {props.name && <p>{props.name}</p>}
-            </div>
-
-            {props.currentBeer && <p>Current Beer: {props.currentBeer}</p>}
-            {props.currentThought && <p>Current Thought: {props.currentThought}</p>}
-            {props.daydream && <img src={props.daydream} alt={props.name + " daydrem"} />}
+                {props.currentBeer && <p>Current Beer: {props.currentBeer}</p>}
+                {props.currentThought && <p>Current Thought: {props.currentThought}</p>}
+                {props.daydream && <img src={props.daydream} alt={props.name + " daydrem"} />}
+            </section>
         </section>
     )
 };
